ea1b452f5997776bff64b7c49f0cac89bd4a8532
Joscelyn Owen <joscelynowen@Joscelyns-iMac.local>
2022-04-08 15:32:59 -0400

    feat: solved maxchar and fizzbuzz

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function findBiggestValue(obj) {
+    let biggest = 0;
+    let biggestKey;
+    Object.keys(obj).forEach((key) => {
+        if (obj[key] > biggest) {
+            biggest = obj[key];
+            biggestKey = key;
+        }
+    })
+    return biggestKey;
+}
+
+
+function maxChar(str) {
+    const chars = {};
+
+    str.split('').forEach((char) => {
+        if (chars[char] !== undefined) {
+            chars[char]++;
+        } else {
+            chars[char] = 1;
+        }
+    })
+    return findBiggestValue(chars)
+
+}
 
 module.exports = maxChar;
