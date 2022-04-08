4663dc22fbe1a1f6eccead6e803a7b86aa55d5fd
Joscelyn Owen <joscelynowen@Joscelyns-iMac.local>
2022-04-08 14:51:26 -0400

    chore: added an additional solution for reversing a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,8 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reverse().join('');
+    // return str.split('').reverse().join('');
+    return str.split('').reduce((rev, char) => char + rev, '' )
 
 }
 
